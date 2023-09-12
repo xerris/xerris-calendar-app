fc329758421e24cd202c1725530596c916a4fa51
R-Ram95 <rohinesh@ualberta.ca>
2023-09-12 16:07:32 -0600

    changed to use vite

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    port: 9003,
+    cors: true,
+    origin: "http://localhost:9003",
+  },
+  build: {
+    rollupOptions: {
+      input: "src/xerris-calendar-app.ts",
+      output: {
+        format: "systemjs",
+      },
+    },
+  },
+});
