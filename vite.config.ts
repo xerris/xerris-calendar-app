eb15138e224f57cdb2bf38f23556e3f0c6e114f1
R-Ram95 <rohinesh@ualberta.ca>
2023-09-26 11:54:38 -0600

    deployment: setup vitest and remove jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -5,6 +7,11 @@ export default defineConfig({
     port: 9003,
     cors: true,
     origin: "http://localhost:9003",
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setup-test.ts",
   },
   build: {
     rollupOptions: {
